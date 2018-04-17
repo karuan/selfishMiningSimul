e2338952b3518ada86c9ee0afb9e4b18706512fe
karuan <kruan@andrew.cmu.edu>
2018-04-17 01:24:49 -0400

    added files simul.py and simulMatch.py

---
+++ simul.py
@@ -0,0 +1,44 @@
+from random import *
+# current functionality: not able to perform match operation
+for total in range(200,2000,200):
+   print total  
+   for adv in range (25,40,1): # adv = percentage of blocks initially given to adversary
+      numSlots = 100; #number of time slots
+      count = 0; # used to count number of trials exceeding threshold
+      numTrials = 10000; # number of trials to get percentage
+      for s in range (0,numTrials):
+         adversary = total*(adv/100.0);
+         honest = ((100.0-adv)/100.0)*total;
+         state = [0,1,0]  #starting state is honest owning genesis block
+         # [a,h, 0 =irrelevant/1=relevant/2=active]
+         for m in range (0,numSlots):
+            a = state[0]
+            h = state[1]
+            randNum = random()
+            alpha = adversary/(honest+adversary)
+            if (h>a): #adopt
+               #reward
+               honest=honest+h
+               #block generation
+               if (randNum< alpha):
+                  state = [1,0,0]
+               else:
+                  state = [0,1,0]
+            #elif (a==1 and h==1 and state[2]==1) : #match (still needs to be implemented)
+            elif (a>h): #override
+               #reward
+               adversary = adversary + h + 1   
+               #block generation
+               if (randNum < alpha):
+                  state = [a-h,0,0]
+               else:      
+                  state = [a-h-1,1,1]
+            else: #wait
+               #block generation
+               if (randNum < alpha):
+                  state = [a+1,h,0] 
+               else:
+                  state = [a,h+1,1]
+         if ((adversary/(honest+adversary)) >= 0.33):
+            count = count + 1
+      print (adv/100.0),",",((count*1.0)/(numTrials*1.0))
