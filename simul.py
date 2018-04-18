fcfac07a81ec3f64f8c356ce0524c82a15370d11
karuan <kruan@andrew.cmu.edu>
2018-04-18 12:53:17 -0400

    changed to use honest strategy when under 0.33 alpha

--- simul.py
+++ simul.py
@@ -3,9 +3,9 @@ from random import *
 for total in range(200,2000,200):
    print total  
    for adv in range (25,40,1): # adv = percentage of blocks initially given to adversary
-      numSlots = 100; #number of time slots
+      numSlots = 10000; #number of time slots
       count = 0; # used to count number of trials exceeding threshold
-      numTrials = 10000; # number of trials to get percentage
+      numTrials = 100; # number of trials to get percentage
       for s in range (0,numTrials):
          adversary = total*(adv/100.0);
          honest = ((100.0-adv)/100.0)*total;
@@ -16,29 +16,37 @@ for total in range(200,2000,200):
             h = state[1]
             randNum = random()
             alpha = adversary/(honest+adversary)
-            if (h>a): #adopt
-               #reward
-               honest=honest+h
-               #block generation
-               if (randNum< alpha):
-                  state = [1,0,0]
+            if (alpha >= 0.33): #use selfish mining strategy
+               if (h>a): #adopt
+                  #reward
+                  honest=honest+h
+                  #block generation
+                  if (randNum< alpha):
+                     state = [1,0,0]
+                  else:
+                     state = [0,1,0]
+               #elif (a==1 and h==1 and state[2]==1) : #match (still needs to be implemented)
+               elif (a>h): #override
+                  #reward
+                  adversary = adversary + h + 1   
+                  #block generation
+                  if (randNum < alpha):
+                     state = [a-h,0,0]
+                  else:      
+                     state = [a-h-1,1,1]
+               else: #wait
+                  #block generation
+                  if (randNum < alpha):
+                     state = [a+1,h,0] 
+                  else:
+                     state = [a,h+1,1]
                else:
-                  state = [0,1,0]
-            #elif (a==1 and h==1 and state[2]==1) : #match (still needs to be implemented)
-            elif (a>h): #override
-               #reward
-               adversary = adversary + h + 1   
-               #block generation
-               if (randNum < alpha):
-                  state = [a-h,0,0]
-               else:      
-                  state = [a-h-1,1,1]
-            else: #wait
-               #block generation
-               if (randNum < alpha):
-                  state = [a+1,h,0] 
-               else:
-                  state = [a,h+1,1]
-         if ((adversary/(honest+adversary)) >= 0.33):
+                  if (randNum < alpha):
+                     adversary=adversary+1
+                     state=[1,0,0]
+                  else:
+                     honest= honest+1
+                     state=[0,1,0]
+            if ((adversary/(honest+adversary)) >= 0.33):
             count = count + 1
       print (adv/100.0),",",((count*1.0)/(numTrials*1.0))
